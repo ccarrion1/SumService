eebfb4b6dec6249de53cb0e1829c700470c575df
carloscrivers@gmail.com <carloscrivers@gmail.com>
2021-05-12 12:49:34 -0500

    Adding logging to the microservice application

--- src/main/java/com/example/sum/SumServiceApplication.java
+++ src/main/java/com/example/sum/SumServiceApplication.java
@@ -1,13 +1,18 @@
 package com.example.sum;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SumServiceApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SumServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SumServiceApplication.class, args);
+		log.info("Application up and running");
 	}
 
 }
--- src/main/java/com/example/sum/services/SumService.java
+++ src/main/java/com/example/sum/services/SumService.java
@@ -1,13 +1,18 @@
 package com.example.sum.services;
 
 import com.example.sum.interfaces.Sum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SumService implements Sum {
 
+    private static final Logger log = LoggerFactory.getLogger(SumService.class);
+
     @Override
     public Double sumNumbers(Double num1, Double num2) {
+        log.debug("The numbers {} and {} are adding up", num1, num2);
         return num1 + num2;
     }
 }
